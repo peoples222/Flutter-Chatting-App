a186ce97259b36ef9449a3ff6d9052c1258c48a2
roigh12 <roigh@naver.com>
2022-06-19 00:31:45 +0900

    첫 화면 생성

---
+++ lib/chatting_room_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChattingRoomScreen extends StatelessWidget {
+  const ChattingRoomScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('asdf');
+  }
+}
